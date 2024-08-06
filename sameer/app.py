8cfcfb601eee89ded4ee579a49a78da44c3b5e73
Sameer Khan A <153911733+sameerkhan2003a@users.noreply.github.com>
2024-08-06 18:34:53 +0530

    Update app.py

--- sameer/app.py
+++ sameer/app.py
@@ -3,3 +3,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))  # Default to port 10000 if PORT is not set
+    app.run(host='0.0.0.0', port=port, debug=True)
